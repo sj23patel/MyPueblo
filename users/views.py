711e2a9e92d4e16a634c34dc22ab06c02b4abfc1
Russell Creswell <rncreswe@ncsu.edu>
2019-10-13 00:55:32 -0400

    Pyrebase added

--- users/views.py
+++ users/views.py
@@ -3,15 +3,39 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyAWiH8XoRRK5UQcc1aLBkMAP0hwFBLQET4",
+    "authDomain": "axial-keep-231806.firebaseapp.com",
+    "databaseURL": "https://axial-keep-231806.firebaseio.com",
+    "projectId": "axial-keep-231806",
+    "storageBucket": "axial-keep-231806.appspot.com",
+    "messagingSenderId": "1005263861742",
+    "appId": "1:1005263861742:web:114c61248c103e9f5480ff",
+    "measurementId": "G-CLLJXNGKBH"
+}
+
+firebase = pyrebase.initialize_app(config)
+
 
 def register(request):
+    print("one")
     if request.method == 'POST':
+        print("two")
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+            print("three")
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            print("email was" + email)
+            auth = firebase.auth()
+            auth.create_user_with_email_and_password(email, password)
+            #username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
+        else:
+            print(form.errors)
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
